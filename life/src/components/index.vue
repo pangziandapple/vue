1613ccc93da9244fe743da4c9ea496492c6b6f11
zhangzhiheng <154369055@qq.com>
2018-12-15 21:19:53 +0800

    登陆页面

--- life/src/components/index.vue
+++ life/src/components/index.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="index">
-    <h1>{{ msg }}</h1>
-    <div class="logindiv">
-        <el-input v-model="input" placeholder="请输入账号！"></el-input>
-    </div>
-    
-    <div  class="logindiv">
-        <el-input v-model="input" type="password" placeholder="请输入密码！"></el-input>
-    </div>
-    
-    <div  class="logindiv">
+  <el-container>
+    <el-header></el-header>
+    <el-main>
       <el-row>
-        <el-button type="primary">登陆</el-button>
-        <el-button type="success">重置</el-button>
+        <el-col :lg="{span: 6,offset: 9}" :md="{span: 12,offset: 6}">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>登录框</span>
+            </div>
+            <div>
+              <el-form :model="form" label-width="80px">
+                <el-form-item label="用户名">
+                  <el-input v-model="form.userName"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                  <el-input type="password" v-model="form.passwd"></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" @click="onSubmit">登录</el-button>
+                  <el-button type="info">取消</el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+          </el-card>
+        </el-col>
       </el-row>
-    </div>
-
-    <div id="registdiv">
-      <a>注册</a>
-    </div>
-  </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -27,28 +34,12 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: '精致的生活'
+      msg: '精致的生活',
+      form: {
+        userName: "张三",
+        passwd: "123456"
+      }
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-a {
-  color: #42b983;
-}
-.index {
-  max-width: 500px;
-  margin:0 auto;
-}
-.logindiv {
-  margin-top:20px;
-}
-
-#registdiv a{
-  float: right;
-  font-size: 15px;
-  margin-right: 10px;
-}
-</style>
